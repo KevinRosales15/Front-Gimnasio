bd4d68aff55a8168b7df5730fc99acbe7147fa90
Kevin Rosales <kevinrosales1511@gmail.com>
2022-05-20 01:50:59 -0600

    Se agregaron los objetivos restantes

--- src/app/components/registro-clientes/registro-clientes.component.ts
+++ src/app/components/registro-clientes/registro-clientes.component.ts
@@ -4,6 +4,8 @@ import { Subscription } from 'rxjs';
 import { FormGroup, FormControl } from '@angular/forms'; 
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { ToastrService } from 'ngx-toastr';
+import { RutinaService } from 'src/app/services/rutina/rutina.service';
+import { rutina } from 'src/app/models/rutinamd';
 
 @Component({
   selector: 'app-registro-clientes',
@@ -24,11 +26,15 @@ export class RegistroClientesComponent implements OnInit {
 
   cliente$: Subscription;
 
-  constructor(private registroClientesService: RegistroClientesService, private toastr: ToastrService) { }
+  constructor(private registroClientesService: RegistroClientesService,private rutinaService: RutinaService,private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
-
+  rutina(id:any){
+    this.cliente$ = this.rutinaService.getRutina(id).subscribe(entry =>{
+      console.log('rutinas', entry);
+    });
+  }
   putDatos() {
     this.cliente$ = this.registroClientesService.putDatos(this.datosClienteForm.value).subscribe(entry => {
       console.log("Resultado", entry);
